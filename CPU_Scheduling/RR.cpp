cc84843c4f32f056cf5f5f2bc5135805102497a5
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-11-05 21:10:33 +0600

    Round Robin CPU Scheduling Algorithm.

---
+++ CPU_Scheduling/RR.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct process
+{
+    int pid, at, bt;
+};
+
+vector<process> p;
+int n, tq; //time quantum
+
+void Round_Robin()
+{
+    vector<int> rt(n), ct(n), tat(n), wt(n), start(n, -1);
+    for (int i = 0; i < n; i++)
+        rt[i] = p[i].bt; // remaining time
+
+    queue<int> q;
+    vector<bool> inQueue(n, false);
+
+    int curr_time = 0, completed = 0;
+
+    sort(p.begin(), p.end(), [](process a, process b){
+        return a.at < b.at;
+    });
+
+    q.push(0);
+    inQueue[0] = true;
+
+    while (completed < n)
+    {
+        if (q.empty())
+        {
+            curr_time++;
+            for (int i = 0; i < n; i++)
+            {
+                if (!inQueue[i] && rt[i] > 0 && p[i].at <= curr_time)
+                {
+                    q.push(i);
+                    inQueue[i] = true;
+                }
+            }
+            continue;
+        }
+
+        int idx = q.front();
+        q.pop();
+
+        // Response Time: record first start time
+        if (start[idx] == -1)
+            start[idx] = curr_time;
+
+        int exec_time = min(rt[idx], tq);
+        rt[idx] -= exec_time;
+        curr_time += exec_time;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!inQueue[i] && rt[i] > 0 && p[i].at <= curr_time)
+            {
+                q.push(i);
+                inQueue[i] = true;
+            }
+        }
+
+        if (rt[idx] == 0)
+        {
+            completed++;
+            ct[idx] = curr_time;
+            tat[idx] = ct[idx] - p[idx].at;
+            wt[idx] = tat[idx] - p[idx].bt;
+        }
+        else
+            q.push(idx);
+    }
+
+    vector<int> resp(n);
+    for (int i = 0; i < n; i++)
+        resp[i] = start[i] - p[i].at; 
+
+    double total_tat = 0, total_wt = 0, total_rt = 0;
+
+    cout << "PID\tAT\tBT\tCT\tTAT\tWT\tRT\n";
+    for (int i = 0; i < n; i++)
+    {
+        total_tat += tat[i];
+        total_wt += wt[i];
+        total_rt += resp[i];
+
+        cout << p[i].pid << "\t" << p[i].at << "\t" << p[i].bt << "\t"
+             << ct[i] << "\t" << tat[i] << "\t" << wt[i]
+             << "\t" << resp[i] << "\n";
+    }
+
+    cout << "Average TAT: " << total_tat / n << "\n";
+    cout << "Average WT:  " << total_wt / n << "\n";
+    cout << "Average RT:  " << total_rt / n << "\n";
+}
+
+int main()
+{
+    cout << "Enter number of processes:\n";
+    cin >> n;
+    p.resize(n);
+
+    cout << "Enter PID AT BT:\n";
+    for (int i = 0; i < n; i++)
+        cin >> p[i].pid >> p[i].at >> p[i].bt;
+
+    cout << "Enter Time Quantum:\n";
+    cin >> tq;
+
+    Round_Robin();
+}
